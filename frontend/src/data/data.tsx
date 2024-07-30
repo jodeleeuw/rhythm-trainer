f33de8caec7f2aec1085ee979b87f1ef27fe01a6
vzhang03 <vzhang@vassar.edu>
2024-07-30 17:17:06 -0400

    Finishing up, moving onto to represent notes

--- frontend/src/data/data.tsx
+++ frontend/src/data/data.tsx
@@ -2,11 +2,11 @@ import Score from "../lib/Score";
 import Measure from "../lib/Measure";
 
 const displayBoxData = [
-  new Measure([4,4], 100, "testing", new Score(['C#4/h', 'E#5/q', 'E#5/q']), 150),
-  new Measure([4,4], 200, "testing", new Score(['C#4/q', 'C#4/q', 'E#5/q', 'E#5/q']), 150),
-  new Measure([4,4], 300, "testing", new Score(['C#5/q', 'B4', 'A4', 'G#4']), 150),
-  new Measure([4,4], 400, "testing", new Score(['C#4/h', 'E#5/q', 'E#5/q']), 150),
-  new Measure([4,4], 500, "testing", new Score(['C#4/h', 'E#5/q', 'E#5/q']), 150),
+  new Measure([4,4], 100, "Beginner", new Score(['C4/h', 'E5/q', 'E5/q']), 150),
+  new Measure([4,4], 200, "Beginner", new Score(['C4/q', 'C4/q', 'E5/q', 'E#5/q']), 150),
+  new Measure([4,4], 300, "Beginner", new Score(['C5/q', 'B4', 'A4', 'G#4']), 150),
+  new Measure([4,4], 400, "Beginner", new Score(['E5/8', 'E5/8', 'C4/h', 'E5/q']), 150),
+  new Measure([4,4], 500, "Beginner", new Score(['C4/h', 'E5/q', 'E5/q']), 150),
 ]
 
 export { displayBoxData };
